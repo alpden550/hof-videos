290f0ed329a15b1ea933d228d0c49b850c875521
alpden550 <alpden550@gmail.com>
2020-06-28 12:08:36 +0300

    Added models, added admin data.

--- halls/admin.py
+++ halls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from halls.models import Hall, Video
+
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
--- halls/models.py
+++ halls/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Hall(models.Model):
+
+    title = models.CharField('Hall Title', max_length=255)
+
+    class Meta:
+        verbose_name = 'Hall'
+        verbose_name_plural = 'Halls'
+
+    def __str__(self):
+        return self.title
+
+
+class Video(models.Model):
+
+    title = models.CharField('Video Title', max_length=255)
+    url = models.URLField('Video URL')
+    youtube_id = models.CharField('YouTube ID', max_length=255)
+    hall = models.ForeignKey(
+        'halls.Hall',
+        verbose_name='Hall',
+        on_delete=models.CASCADE,
+        related_name='videos',
+    )
+
+    class Meta:
+        verbose_name = 'Video'
+        verbose_name_plural = 'Videos'
+
+    def __str__(self):
+        return self.title
