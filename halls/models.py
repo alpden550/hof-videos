6cab94867dccd44442d793b88d998dbb2257f3e0
alpden550 <alpden550@gmail.com>
2020-06-28 18:52:25 +0300

    Updated creating hall.

--- halls/models.py
+++ halls/models.py
@@ -1,9 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Hall(models.Model):
 
     title = models.CharField('Hall Title', max_length=255)
+    user = models.ForeignKey(
+        get_user_model(),
+        verbose_name='User',
+        on_delete=models.CASCADE,
+        related_name='halls',
+    )
 
     class Meta:
         verbose_name = 'Hall'
--- halls/views.py
+++ halls/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import CreateView, TemplateView
-from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
 from halls.models import Hall
 
@@ -19,8 +20,12 @@ class UserSignUpView(CreateView):
     success_url = reverse_lazy('home')
 
 
-class HallCreateView(CreateView):
+class HallCreateView(LoginRequiredMixin, CreateView):
     model = Hall
     fields = ('title', )
     template_name = 'halls/create_hall.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
