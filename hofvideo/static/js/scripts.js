a8827960b1769fb8fc2fc8519e4477154b3f24a8
alpden550 <alpden550@gmail.com>
2020-07-06 19:22:18 +0300

    Added feature adding video from searching.

--- hofvideo/static/js/scripts.js
+++ hofvideo/static/js/scripts.js
@@ -21,7 +21,8 @@ $('#id_search').keyup(function () {
         data['items'].forEach(function(video) {
           results += '<div class="col-md-4 mt-3"><div class="card mb-4 shadow-sm">';
           results += '<iframe width="100%" height="200" src="https://www.youtube.com/embed/'+video['id']['videoId']+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-          results += '</div></div>'
+          results += '<div class="card-body"><p class="card-text">'+video['snippet']['title']+'</p>'
+          results += '<a href="#" class="btn btn-primary" onclick="addVideo(\'' +video['id']['videoId']+ '\')">Add</a></div></div></div>';
         });
 
         results += '</div>'
@@ -31,3 +32,8 @@ $('#id_search').keyup(function () {
     });
   }, 1000);
 });
+
+function addVideo(video_id) {
+  $('#id_url').val('https://www.youtube.com/watch?v=' + video_id);
+  $('#submit_video').submit();
+}
